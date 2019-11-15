d40760644119d15c24ea7b1e1f6a1e14a1d2bb72
lukavec <57807443+lukavec@users.noreply.github.com>
2019-11-15 22:08:22 +0100

    kng

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const Discord = require("discord.js");
+const fs = require("fs");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+})
+
+client.on('message', msg => {
+  if(msg.content.startsWith('/kick') && msg.mentions.members.first().id==644968501118894094) {
+    //msg.reply(msg.mentions.members.first().id);
+    msg.reply("I will NOT kick myself!!!!!!!!!!!!");
+  }
+  else if (msg.content.startsWith('/kick')) {
+    msg.reply('Give me 1 reason WHY!');
+    //msg.reply(msg.content)
+  }
+})
+
+client.login("NjQ0OTY4NTAxMTE4ODk0MDk0.Xc7wSg.Ku9AcciOrmAUBqXhwL9UT6uHy1o");
