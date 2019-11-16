25ccc5b26ae15a5a33d39583d6aa4ce6eb90f6a4
lukavec <57807443+lukavec@users.noreply.github.com>
2019-11-16 19:58:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const client = new Discord.Client();
 // where your node app starts
 
 // init project
-console.log(process.env.TOKEN)
-client.login(process.env.TOKEN);
+console.log(secrets.TOKEN)
+client.login(secrets.TOKEN);
 //const bodyParser = require("body-parser");
 /*const app = express();
 
